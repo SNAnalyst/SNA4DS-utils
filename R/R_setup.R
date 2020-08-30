a447d6a4575a093c0643d372c67c13c7d7858bed
SNAnalyst <62701729+SNAnalyst@users.noreply.github.com>
2020-08-31 00:18:57 +0200

    updated package list

--- R/R_setup.R
+++ R/R_setup.R
@@ -1,1 +1,11 @@
-hopla
+
+pkgs <- c("igraph",
+          "igraphdata",
+           "influenceR",
+           "intergraph",
+           "magrittr",
+           "network",
+           "remotes",
+           "sna")
+
+install.packages(pkgs = pkgs, dependencies = TRUE)
