a33e53475a7f4591cab1da13ba86a1e70b74fae0
Zucca <c.zucca@tue.nl>
2020-09-08 18:18:28 +0200

    homework week 3

---
+++ R/Lab3_homework.R
@@ -0,0 +1,50 @@
+
+# Homework Lab 3 -Measures I
+
+# to read data into R:
+# - make sure you set the pathway to the folder where you are storing the data 
+# setwd()
+load("Copper_Ore.RData")
+
+# This data contains information about the international trade of copper ore in 2009.
+# Nodes are countries.
+# Ties tell you whether two countries traded copper together
+ 
+
+# 1) print out information about the data set 
+# how many nodes?
+# how many edges?
+# is it directed or undirected?
+# which attributes?
+# is it uni-partite?
+
+
+# 2) Calculate the node degree appropriate for this kind of network and provide 
+# the labels of the nodes with the highest degree.
+
+# 3) What does node degree mean in this network?
+
+# 4) Calculate the shortest path between the node with the highest degree 
+# and the other four with the highest degree after it.
+
+# 5) Are the 5 nodes with the highest degree close or far in network terms?
+
+# 6) Calculate the diameter of the network
+
+# 7) Calculate the average path length
+
+# 8) Calculate the density of the network
+
+# 9) Are there any cliques? If yes, how many nodes has the largest?
+
+# 10) you have information about one network attribute. In which way this 
+# attribute helps understanding what happens in this network?
+
+# 11) subset two subgraphs that you think are relevant and tell me why
+
+# 12) put the previous two sub-graphs together and plot them in a readable way
+
+# 13) add labels to your plot
+
+# 14) add the attribute to your plot
+
